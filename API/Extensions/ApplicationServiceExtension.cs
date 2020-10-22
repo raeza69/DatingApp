9708c561b4af1a3d0edc6bccd819c2aa4e34614d
unknown <raeza151@gmail.com>
2020-10-22 23:35:32 +0800

    End of section 5

--- API/Extensions/ApplicationServiceExtension.cs
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -19,4 +19,5 @@ namespace API.Extensions
 
             return services;
         }   
+    }
 }
---
+++ client/src/app/_models/user.ts
@@ -0,0 +1,6 @@
+import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
+
+export interface User {
+    username: string;
+    token: string;
+}
